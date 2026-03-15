fec149931350f93b5d69d4bbeba8cfc1a96b1c8f
Emre Pehlivan <emrepehli@gmail.com>
2026-03-15 18:19:19 +0300

    Initialize EntityBase with a new GUID creation method in the constructor to ensure unique identifiers for entities.

--- src/BuildingBlocks/PharmaStock.BuildingBlocks/Entities/EntityBase.cs
+++ src/BuildingBlocks/PharmaStock.BuildingBlocks/Entities/EntityBase.cs
@@ -3,4 +3,9 @@ namespace PharmaStock.BuildingBlocks.Entities;
 public abstract class EntityBase : IEntity
 {
     public Guid Id { get; protected init; }
+
+    protected EntityBase()
+    {
+        Id = Guid.CreateVersion7();
+    }
 }
